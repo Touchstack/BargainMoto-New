b0853fc4705f74f9bc072fde95298c3a9dfdf290
Okunola Mary Oluwabunmi <123972532+Ewabunmi@users.noreply.github.com>
2023-08-22 03:35:38 -0700

    Font Setup
    Montserrat fonts

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+     "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: { 
+      fontFamily: {
+        Thin: ["Montserrat-Thin", "sans-serif"],
+        ExtraLight: ["Montserrat-ExtraLight", "sans-serif"],
+        Light: ["Montserrat-Light", "sans-serif"],
+        Bold: ["Montserrat-Bold", "sans-serif"],
+        Regular: ["Montserrat-Regular", "sans-serif"],
+        SemiBold: ["Montserrat-SemiBold", "sans-serif"],
+      },
+    },
+  },
+
+  plugins: [],
+}
+
