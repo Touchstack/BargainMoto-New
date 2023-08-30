38d1b87c28fbf737ac54e1948c2536da056302b6
Okunola Mary Oluwabunmi <123972532+Ewabunmi@users.noreply.github.com>
2023-08-30 04:33:36 -0700

    ContactHeroSection
    About-ContactHeroSection

--- src/components/Contact/ContactHeroSection.jsx
+++ src/components/Contact/ContactHeroSection.jsx
@@ -3,19 +3,12 @@ import Navbar from "../Navbar/Navbar";
 
 const ContactHeroSection = () => {
   return (
-//     <div className=" bg-[#8C394C] lg:pt-2 md:pt-8 sm:px-4 py-40">
-//       <Navbar/>
-//       <div className="m-24 font-Bold text-5xl max-w-[800px] p-9 text-[#FFFFFF]"> 
-//        We will love to hear from you.
-//         </div>
-//     </div>
-
-<div className=" bg-[#8C394C] lg:pt-2 md:pt-8 sm:px-4 py-40 pb-10">
-<Navbar/>
-<div className="m-12 font-Bold text-5xl max-w-[600px] p-9 text-[#FFFFFF]"> 
-We will love to hear from you.
-</div>
-</div>
+      <div className=" bg-[#8C394C] lg:pt-2 md:pt-8 sm:px-4 py-40 pb-10">
+      <Navbar/>
+      <div className="m-12 font-Bold text-5xl max-w-[600px] p-9 text-[#FFFFFF]"> 
+       We will love to hear from you.
+      </div>
+      </div>
    )
 }
 
