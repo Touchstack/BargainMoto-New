87ef240d6521c9af4f56643aca72de44a3f77eaa
Okunola Mary Oluwabunmi <123972532+Ewabunmi@users.noreply.github.com>
2023-09-04 06:39:29 -0700

    Update To Home Page, Contact Us and About
    Update To Home Page, Contact Us and About

---
+++ src/page/AboutPage/AboutPage.jsx
@@ -0,0 +1,25 @@
+import AboutHeroSection from '../../components/About/AboutHeroSection'
+import HowWeDoIt from '../../components/About/HowWeDoIt'
+import Footer from '../../components/Footer/Footer'
+
+const AboutPage = () => {
+  return (
+    <div>
+        <AboutHeroSection/>
+         <HowWeDoIt/>
+         <Footer/>
+         </div>
+     )
+   }
+ export default AboutPage
+
+
+
+
+
+
+
+
+
+
+
