fd59e37afc6ba6e98f8121a9ff61e805476c6cd9
Eugene Dumoga <eugenedumoga@gmail.com>
2023-09-07 16:08:30 +0000

    Featured Listings & Updates to UI

---
+++ src/components/Cards/FeaturedListings.jsx
@@ -0,0 +1,64 @@
+import { Fragment } from "react";
+import PrimaryButton from "../Buttons/PrimaryButton";
+import ListingsCard from "./ListingsCard";
+import PropTypes from "prop-types";
+
+const FeaturedListings = ({ vehicleData, loading }) => {
+  return (
+    <div className="bg-[#FBF8FC]">
+      <div className="container mx-auto text-center lg:py-24 md:py-24 sm:py-12 py-12 md:px-4 px-11">
+        <h3 className="font-Bold tracking-tight lg:text-4xl md:text-3xl sm:text-3xl text-3xl lg:py-8 md:py-8 py-4">
+          Featured Listings
+        </h3>
+
+        <p className="font-Regular text-lg mx-auto mb-16 lg:w-4/6 md:w-5/6 w-6/6">
+          Check out the top-notch cars available right now on BargainMoto. Check
+          out some of the best deals we've got!
+        </p>
+
+        {vehicleData.length >= 1 ? (
+          <Fragment>
+            <div className="grid lg:grid-cols-3  md:grid-cols-3 sm:grid-cols-1 grid-cols-1 gap-4 text-left md:place-items-stretch place-items-center">
+              {vehicleData.map((e, i) => {
+                return (
+                  <div key={i}>
+                    <ListingsCard
+                      loading={loading}
+                      sellingPrice={e?.selling_price}
+                      uuid={e.uuid}
+                      IMG={`https://bargain-moto.nyc3.digitaloceanspaces.com/${e?.featured_image}`}
+                      title={e?.year + " " + e?.color + " " + e?.name}
+                      description={e?.sale_type}
+                      iconText={e?.location}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+            <div className="mt-12 lg:mb-12 md:mb-10 sm:mb-10 mb-10">
+              <a href="/car-listings">
+                <PrimaryButton buttonText={"View more listings"} />
+              </a>
+            </div>
+          </Fragment>
+        ) : (
+          <p className="font-Bold text-gray-400 lg:text-2xl md:text-2xl sm:text-xl text-xl lg:py-12 md:py-8 py-4 bg-gray-100 rounded-lg">
+            No Available Featured Listings
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+FeaturedListings.propTypes = {
+  vehicleData: PropTypes.array,
+  loading: PropTypes.bool,
+};
+
+FeaturedListings.defaultProps = {
+  vehicleData: [],
+  loading: "",
+};
+
+export default FeaturedListings;
--- src/components/Hero/HomeCustomerVoices.jsx
+++ src/components/Hero/HomeCustomerVoices.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import FrameIcon from "../../assets/images/Frame.svg";
 import Badge from "../../assets/images/Verified-badge.svg";
 import EllipseIcon from "../../assets/images/Ellipse.svg";
@@ -13,115 +12,118 @@ const HomeCustomerVoices = () => {
         Dont just take our word for it. Hear what our happy customers
         <p>have to say about BargainMoto! </p>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 relative gap-x-2 gap-y-16 px-4 pt-12 sm:pt-20 text-block text-[#191919]">
-        <div className="bg-[#FDFCFB] rounded-lg shadow-2xl w-[304px] h-[257px] ml-64">
-          <div className="font-Regular text-[#191919] text-lg mt-4 pl-4 pr-4">
-            "I was nervous about buying a car online, but BargainMoto made it
-            easy. I found a quality car that fits my budget. Highly recommend."
-            <p className="inline-flex mt-9 text-base text-[#666]">
-              <img
-                src={EllipseIcon}
-                alt="Ellipse"
-                style={{ height: "40px", width: "40px" }}
-                className="img-fluid mx-auto mr-2"
-              />
-              <span className="text-[#191919]">
-                Dee Nti, <span className="text-[#666]">Verified Seller</span>
-                <div className="flex gap-x-2 h-max">
-                  <img
-                    src={FrameIcon}
-                    alt="Frame"
-                    style={{ height: "16px", width: "14px" }}
-                    className=""
-                  />
-                  <img
-                    src={FrameIcon}
-                    alt="Frame"
-                    style={{ height: "16px", width: "14px" }}
-                    className=""
-                  />
-                  <img
-                    src={FrameIcon}
-                    alt="Frame"
-                    style={{ height: "16px", width: "14px" }}
-                    className=""
-                  />
-                  <img
-                    src={FrameIcon}
-                    alt="Frame"
-                    style={{ height: "16px", width: "14px" }}
-                    className=""
-                  />
-                  <img
-                    src={FrameIcon}
-                    alt="Frame"
-                    style={{ height: "16px", width: "14px" }}
-                    className=""
-                  />
-                </div>
-              </span>
-              <img
-                src={Badge}
-                alt="Verified-badge"
-                style={{ height: "16px", width: "16px" }}
-                className="img-fluid mx-auto ml-3 mt-1"
-              />
-            </p>
+      <div className="container mx-auto">
+        <div className="flex lg:flex-row md:flex-row sm:flex-col flex-col justify-center items-center  px-4 pt-12 sm:pt-20 text-block text-[#191919]">
+          <div className="bg-[#FDFCFB] rounded-lg shadow-2xl w-[304px] h-[257px] lg:mr-5 md:mr-5 md:mb-0 sm:mb-5 mb-5">
+            <div className="font-Regular text-[#191919] text-lg mt-4 pl-4 pr-4">
+              "I was nervous about buying a car online, but BargainMoto made it
+              easy. I found a quality car that fits my budget. Highly
+              recommend."
+              <p className="inline-flex mt-9 text-base text-[#666]">
+                <img
+                  src={EllipseIcon}
+                  alt="Ellipse"
+                  style={{ height: "40px", width: "40px" }}
+                  className="img-fluid mx-auto mr-2"
+                />
+                <span className="text-[#191919]">
+                  Dee Nti, <span className="text-[#666]">Verified Seller</span>
+                  <div className="flex gap-x-2 h-max">
+                    <img
+                      src={FrameIcon}
+                      alt="Frame"
+                      style={{ height: "16px", width: "14px" }}
+                      className=""
+                    />
+                    <img
+                      src={FrameIcon}
+                      alt="Frame"
+                      style={{ height: "16px", width: "14px" }}
+                      className=""
+                    />
+                    <img
+                      src={FrameIcon}
+                      alt="Frame"
+                      style={{ height: "16px", width: "14px" }}
+                      className=""
+                    />
+                    <img
+                      src={FrameIcon}
+                      alt="Frame"
+                      style={{ height: "16px", width: "14px" }}
+                      className=""
+                    />
+                    <img
+                      src={FrameIcon}
+                      alt="Frame"
+                      style={{ height: "16px", width: "14px" }}
+                      className=""
+                    />
+                  </div>
+                </span>
+                <img
+                  src={Badge}
+                  alt="Verified-badge"
+                  style={{ height: "16px", width: "16px" }}
+                  className="img-fluid mx-auto ml-3 mt-1"
+                />
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="bg-[#FDFCFB] rounded-lg shadow-2xl w-[304px] h-[257px]">
-          <div className="font-Regular text-[#191919] text-lg mt-4 pl-4 pr-4">
-            "Great selection, fair prices, and superb customer service.
-            BargainMoto is my go-to for all car deals."
-            <p className="inline-flex mt-16 text-base text-[#666]">
-              <img
-                src={EllipseIcon2}
-                alt="Amina"
-                style={{ height: "40px", width: "40px" }}
-                className="img-fluid mx-auto mr-2"
-              />
-              <span className="text-[#191919]">
-                Amina, <span className="text-[#666]">Verified Buyer</span>
-                <div className="flex gap-x-2 h-max">
-                  <img
-                    src={FrameIcon}
-                    alt="Frame"
-                    style={{ height: "16px", width: "14px" }}
-                    className=""
-                  />
-                  <img
-                    src={FrameIcon}
-                    alt="Frame"
-                    style={{ height: "16px", width: "14px" }}
-                    className=""
-                  />
-                  <img
-                    src={FrameIcon}
-                    alt="Frame"
-                    style={{ height: "16px", width: "14px" }}
-                    className=""
-                  />
-                  <img
-                    src={FrameIcon}
-                    alt="Frame"
-                    style={{ height: "16px", width: "14px" }}
-                    className=""
-                  />
-                  <img
-                    src={FrameIcon}
-                    alt="Frame"
-                    style={{ height: "16px", width: "14px" }}
-                    className=""
-                  />
-                </div>
-              </span>
-              <img
-                src={Badge}
-                alt="Verified-badge"
-                style={{ height: "16px", width: "16px" }}
-                className="img-fluid mx-auto ml-3 mt-1"
-              />
-            </p>
+          <div className="bg-[#FDFCFB] rounded-lg shadow-2xl w-[304px] h-[257px]">
+            <div className="font-Regular text-[#191919] text-lg mt-4 pl-4 pr-4">
+              "Great selection, fair prices, and superb customer service.
+              BargainMoto is my go-to for all car deals."
+              <p className="inline-flex mt-16 text-base text-[#666]">
+                <img
+                  src={EllipseIcon2}
+                  alt="Amina"
+                  style={{ height: "40px", width: "40px" }}
+                  className="img-fluid mx-auto mr-2"
+                />
+                <span className="text-[#191919]">
+                  Amina, <span className="text-[#666]">Verified Buyer</span>
+                  <div className="flex gap-x-2 h-max">
+                    <img
+                      src={FrameIcon}
+                      alt="Frame"
+                      style={{ height: "16px", width: "14px" }}
+                      className=""
+                    />
+                    <img
+                      src={FrameIcon}
+                      alt="Frame"
+                      style={{ height: "16px", width: "14px" }}
+                      className=""
+                    />
+                    <img
+                      src={FrameIcon}
+                      alt="Frame"
+                      style={{ height: "16px", width: "14px" }}
+                      className=""
+                    />
+                    <img
+                      src={FrameIcon}
+                      alt="Frame"
+                      style={{ height: "16px", width: "14px" }}
+                      className=""
+                    />
+                    <img
+                      src={FrameIcon}
+                      alt="Frame"
+                      style={{ height: "16px", width: "14px" }}
+                      className=""
+                    />
+                  </div>
+                </span>
+                <img
+                  src={Badge}
+                  alt="Verified-badge"
+                  style={{ height: "16px", width: "16px" }}
+                  className="img-fluid mx-auto ml-3 mt-1"
+                />
+              </p>
+            </div>
           </div>
         </div>
       </div>
