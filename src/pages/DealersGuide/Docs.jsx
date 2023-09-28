da0021bd0e8fb436040498ac6ab9aa5feeeac734
Okunola Mary Oluwabunmi <123972532+Ewabunmi@users.noreply.github.com>
2023-09-28 03:45:35 -0700

    Navigation for Dealer Guide
    Navigation for Dealer Guide

---
+++ src/pages/DealersGuide/Docs.jsx
@@ -0,0 +1,61 @@
+export const Docs = {
+  buyer: {
+    title: "Buying a Car",
+    subTitle: "How it works.",
+    steps: [
+      {
+        title: "1. Search and Register",
+        details:
+          "Find cars on our site and register to make an offer. Keep track of your offers all in one place. When you sign up on our platform and create your account, you can choose to be partially verified (ID verification only) or fully verified (ID verification plus verification of funds). Being fully verified gives you greater access to cars and sellers on the platform as some sellers only prefer to engage with fully verified buyers.",
+      },
+      {
+        title: "2. Bargain with sellers",
+        details:
+          "See a car within your budget that you like? Review the car’s full mechanical report and bid for it. You will receive direct feedback from the seller through the platform whether your bid has been accepted or rejected. If your initial bid is rejected you can make a new bid up to a number of bids set by the seller, until you reach an agreement on the price. We verify the identities of all sellers on our platform to make sure that you are only bargaining with the legal owner of the vehicle. This way we ensure a safer experience for everyone.",
+      },
+      {
+        title: "3. Schedule test drive",
+        details:
+          "While we do everything we can to make buying a car on Bargain Moto as safe and easy as possible (with our vehicle pre-purchase inspection and seller verification) it’s ultimately your responsibility to perform your own due diligence and make sure that the car you’re considering is right for you. Easily schedule a date and time to test drive a car through the platform with a click of a button!",
+      },
+      {
+        title: "4. Make payment and sign Bill of Sale",
+        details:
+          "When you and the seller have come to an agreement and are ready to close the deal, you make payment in full for the vehicle through Bargain Moto. Once we have confirmed funds, we arrange for both parties to sign the Bill of Sale and get the car delivered to you before the payment is transferred to the seller.  ",
+      },
+    ],
+  },
+  seller: {
+    title: "Selling a Car",
+    subTitle: "How it works.",
+    steps: [
+      {
+        title: "1. Register and get Verified",
+        details:
+          "To make Bargain Moto safe for everyone we require a few details like your email, phone number, ID verification and car ownership documents (Vehicle Registration Certificate)",
+      },
+      {
+        title: "2. List your Vehicle",
+        details:
+          "Simply create your listing with our step-by-step guide. Take excellent photos of your car to get the best offer. We recommend filming a quick video in landscape mode of the car from all angles, as well as an engine start, and upload to our YouTube page.",
+      },
+      {
+        title: "3. Set the terms",
+        details:
+          "It’s your car so you are in control.  You set the price and the number of bids you want to receive from each bidder. You can choose to receive offers from only fully verified buyers (ID verification plus verified availability of funds) or from also partially verified buyers (ID Verification only) as well. All these features ensure that you do not waste your time dealing with fraudsters or unserious buyers.",
+      },
+      {
+        title: "4. Bargain and schedule test drives",
+        details:
+          "Manage all offers through your account with the ability to accept or reject a bid, and schedule test drives with buyers who have committed to the transaction.",
+        others:
+          "Remember - Timely and positive seller participation in the negotiating process is crucial to a successful deal. Good seller participation will result in more bids and a higher final sale price. Successful bidders may contact you directly via the “Contact Seller” feature, allowing them to schedule test drives and ask additional questions.",
+      },
+      {
+        title: "5. Sign Bill of Sale and get Paid",
+        details:
+          "After you and the buyer have agreed on the sale price and signed the Bill of Sale, we collect payment from the buyer, ensure that the car is delivered to the buyer, and then transfer the payment to you electronically.",
+      },
+    ],
+  },
+};
